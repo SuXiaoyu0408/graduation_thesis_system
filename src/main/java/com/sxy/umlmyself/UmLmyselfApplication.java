690b477afb0b9d11e619ea5d850761db9b137fe8
SuXiaoyu0408 <1921702199@qq.com>
2025-12-28 10:08:20 +0800

    新建UML的springboot项目

---
+++ src/main/java/com/sxy/umlmyself/UmLmyselfApplication.java
@@ -0,0 +1,13 @@
+package com.sxy.umlmyself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmLmyselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmLmyselfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sxy/umlmyself/UmLmyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxy.umlmyself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmLmyselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
